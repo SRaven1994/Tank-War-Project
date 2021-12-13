660f5eb40e5fe1ed253da1cf7b445480d5a3e0d9
SRaven1994 <92119448+SRaven1994@users.noreply.github.com>
2021-12-13 16:12:19 +0000

    Today work completed for the day

--- js/baseTank.js
+++ js/baseTank.js
@@ -1,30 +1,43 @@
-class BaseTank{
+class BaseTank {
     /** @type {number} */
     tankSpeed = 100
     /** @type {number} */
     damageMax = 2
-    constructor(Scene, x, y, texture, frame){
+    constructor(scene, x, y, texture, frame) {
+        this.scene = scene
+        this.shadow = this.scene.add.image(x, y, texture, "shadow")
+        this.shadow.setDepth(1)
+        this.hull = this.scene.physics.add.sprite(x, y, texture, frame)
+        this.hull.setSize(this.hull.width - 8, this.hull.height - 8)
+        this.hull.setDepth(2)
+        this.hull.body.collideWorldBounds = true
+        this.hull.body.bounce.setTo(1, 1)
+        this.turret = this.scene.physics.add.sprite(x, y, texture, "turret")
+        this.turret.setDepth(4)
+        this.damageCount = 0
+    }
+    update() {
+        this.shadow.x = this.turret.x = this.hull.x
+        this.shadow.y = this.turret.y = this.hull.y
+        this.shadow.rotation = this.hull.rotation
 
     }
-    update(){
+    damage() {
 
     }
-    damage(){
+    setBullets() {
 
     }
-    setBullets(){
+    burn() {
 
     }
-    burn(){
+    isDestroyed() {
 
     }
-    isDestroyed(){
+    isImobilised() {
 
     }
-    isImobilised(){
-
-    }
-    enableCollision(){
-
+    enableCollision(destructLayer) {
+        this.scene.physics.add.collider(this.hull, destructLayer)
     }
 }
