1ddd8034e7afbdc3a1f9f6c898656a664a025ef4
SRaven1994 <92119448+SRaven1994@users.noreply.github.com>
2021-12-13 12:38:32 +0000

    Add files to Project

---
+++ js/baseTank.js
@@ -0,0 +1,30 @@
+class BaseTank{
+    /** @type {number} */
+    tankSpeed = 100
+    /** @type {number} */
+    damageMax = 2
+    constructor(Scene, x, y, texture, frame){
+
+    }
+    update(){
+
+    }
+    damage(){
+
+    }
+    setBullets(){
+
+    }
+    burn(){
+
+    }
+    isDestroyed(){
+
+    }
+    isImobilised(){
+
+    }
+    enableCollision(){
+
+    }
+}
